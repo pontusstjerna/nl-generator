cf377688128a74b1106501824eeddb53c1e854df
Pontus Stjernström <pontus@stjernstrom.nu>
2019-12-14 23:18:46 +0100

    oh and also add build script and remove build files :)

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _generatorService = require("./generatorService");
-
-// TODO: Add API :D
-
-(0, _generatorService.printText)(50, "För kräftan krävs det mycket energi");
-(0, _generatorService.printText)(100);
