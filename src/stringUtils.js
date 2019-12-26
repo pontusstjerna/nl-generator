2aa916b47b8d98cafba751440119ae221928f67f
Pontus Stjernström <pontus@stjernstrom.nu>
2019-12-26 19:06:58 +0100

    some code clean up

--- src/stringUtils.js
+++ src/stringUtils.js
@@ -2,20 +2,10 @@ const sentenceSeparators = [".", "!", "?"];
 
 const capitalizeFirst = input => input.charAt(0).toUpperCase() + input.substring(1);
 
-const split = (input, separators) => {
-    let result = input.split(separators[0]);
-    separators.forEach(separator => {
-        result = result.flatMap(part => part.split(separator));
-    });
-
-    return result;
-};
-
-const splitJoin = (input, divider, fn) => input.split(divider).map(fn).join(divider);
-
 export const capitalizeSentences = input => {
     sentenceSeparators.forEach(separator => {
-       input = splitJoin(input, separator + " ", capitalizeFirst);
+        separator = separator + " ";
+        input = input.split(separator).map(capitalizeFirst).join(separator);
     });
     return input;
 };
